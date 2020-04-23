22777d163e2cf864d44636f28ba24d54868ea719
Sheetal Sathyanarayana Prasad <60914044+sheetal-prasad@users.noreply.github.com>
2020-04-23 15:47:33 -0500

    H1B dependent and states code update

--- clean_wage.py
+++ clean_wage.py
@@ -7,6 +7,7 @@ def clean_wages(w):
     if isinstance(w, str):
         return(w.replace('$', '').replace(',', ''))
     return(w)
+
 def remove_num(text):
     """
     Function to clean JOB_TITLE and SOC_TITLE by removing digits from the values
@@ -24,6 +25,10 @@ def drop_less_significant(cleaned):
     cleaned = cleaned.groupby("SOC_TITLE").filter(lambda x: len(x) > 15)
     cleaned = cleaned.groupby("EMPLOYER_NAME").filter(lambda x: len(x) > 15)
     cleaned = cleaned.groupby("WORKSITE_STATE").filter(lambda x: len(x) > 15)
+    cleaned=cleaned.groupby("SOC_CODE").filter(lambda x: len(x)>15)
+    cleaned=cleaned.groupby("SOC_TITLE").filter(lambda x: len(x)>15)
+    cleaned=cleaned.groupby("EMPLOYER_NAME").filter(lambda x: len(x)>15)
+    cleaned=cleaned.groupby("WORKSITE_STATE").filter(lambda x: len(x)>15)
     return cleaned
     
 def wage_feature_eng(wage):
@@ -32,6 +37,7 @@ def wage_feature_eng(wage):
     into categorical data
     """
     if wage <=50000:
+    if wage <= 50000:
         return "VERY LOW"
     elif wage in range(50000,75000):
         return "LOW"
@@ -40,6 +46,7 @@ def wage_feature_eng(wage):
     elif wage in range(100000,150000):
         return "HIGH"
     elif wage >=150000:
+    elif wage >= 150000:
         return "VERY HIGH"
 
 def clean_wageUnit(np,cleaned):
@@ -51,6 +58,10 @@ def clean_wageUnit(np,cleaned):
     cleaned['WAGES'] = np.where(cleaned['WAGE_UNIT_OF_PAY'] == 'Hour',cleaned['WAGES'] * 2080,cleaned['WAGES']) # 2080=8 hours*5 days* 52 weeks
     cleaned['WAGES'] = np.where(cleaned['WAGE_UNIT_OF_PAY'] == 'Bi-Weekly',cleaned['WAGES'] *26,cleaned['WAGES'])
     cleaned['WAGES'] = np.where(cleaned['WAGE_UNIT_OF_PAY'] == 'Week',cleaned['WAGES'] * 52,cleaned['WAGES'])
+    cleaned['WAGES']=np.where(cleaned['WAGE_UNIT_OF_PAY']=='Month',cleaned['WAGES']*12,cleaned['WAGES'])
+    cleaned['WAGES']=np.where(cleaned['WAGE_UNIT_OF_PAY']=='Hour',cleaned['WAGES']*2080,cleaned['WAGES']) # 2080=8 hours*5 days* 52 weeks
+    cleaned['WAGES']=np.where(cleaned['WAGE_UNIT_OF_PAY']=='Bi-Weekly',cleaned['WAGES']*26,cleaned['WAGES'])
+    cleaned['WAGES']=np.where(cleaned['WAGE_UNIT_OF_PAY']=='Week',cleaned['WAGES']*52,cleaned['WAGES'])
     return cleaned
 
 
@@ -133,6 +144,29 @@ def clean_states(cleaned):
     cleaned.loc[(cleaned.WORKSITE_STATE == "DC"),"WORKSITE_STATE"] = "DISTRICT OF COLUMBIA"
     cleaned.loc[(cleaned.WORKSITE_STATE == "CT"),"WORKSITE_STATE"] = "CONNECTICUT"
     
+    """
+    Function to change the WORKSITE_STATE cplumn some values are abbrevation,
+    some values are state names, we are replacing with abbrevations wiith state
+    names
+    """
+    state={"AL":"ALABAMA","AK":"ALASKA","AZ":"ARIZONA","AR":"ARKANSAS","CA":"CALIFORNIA","CO":"COLORADO","DE":"DELAWARE",\
+       "FL":"FLORIDA","GA":"GEORGIA","HI":"HAWAII","ID":"IDAHO","IL":"ILLINOIS","IN":"INDIANA","IA":"IOWA","KS":"KANSAS",\
+       "KY":"KENTUCKY","LA":"LOUISIANA","ME":"MAINE","MD":"MARYLAND","MA":"MASSACHUSETTS","MI":"MICHIGAN","MN":"MINNESOTA",\
+       "MS":"MISSISSIPPI","MO":"MISSOURI","MT":"MONTANA","NE":"NEBRASKA","NV":"NEVADA","NH":"NEW HAMPSHIRE","NJ":"NEW JERSEY",\
+       "NM":"NEW MEXICO","NY":"NEW YORK","NC":"NORTH CAROLINA","ND":"NORTH DAKOTA","OH":"OHIO","OK":"OKLAHOMA","OR":"OREGON",\
+       "PA":"PENNSYLVANIA","RI":"RHODE ISLAND","SC":"SOUTH CAROLINA","SD":"SOUTH DAKOTA","TN":"TENNESSEE","TX":"TEXAS",\
+       "UT":"UTAH","VT":"VERMONT","VA":"VIRGINIA","WA":"WASHINGTON","WV":"WEST VIRGINIA","WI":"WISCONSIN","WY":"WYOMING",\
+       "PR":"PUERTO RICO","VI":"U.S. VIRGIN ISLANDS","MP":"NORTHERN MARIANA ISLANDS","GU":"GUAM","MH":"MARSHALL ISLANDS",\
+       "PW":"PALAU","DC":"DISTRICT OF COLUMBIA","CT":"CONNECTICUT"}
+    cleaned.replace({"WORKSITE_STATE": state},inplace = True)
+    return cleaned
+
+def cat_to_num(cleaned):
+    """
+    Function to convert categorical values into numerical values to use for classifiers
+    """
+    cleaned.loc[(cleaned.H1B_DEPENDENT=="Y"),"H1B_DEPENDENT"] = 1
+    cleaned.loc[(cleaned.H1B_DEPENDENT=="N"),"H1B_DEPENDENT"] = 0
     return cleaned
 
     
